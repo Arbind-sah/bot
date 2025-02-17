5ce83bc0121662845d9c3b0d8bf67c626e877878
Arbind sah <sah.arbin735@gmail.com>
2025-02-17 12:54:06 +0545

    changed

--- app.py
+++ app.py
@@ -144,8 +144,8 @@ st.markdown("""
     left: 0;
     bottom: 0;
     width: 100%;
-    background-color:red;
-    color: white;
+    background-color: #333;
+    color: #FF0000;
     text-align: center;
     padding: 10px;
 }
